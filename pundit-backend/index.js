002a143c882d04768e641df5f6e69d25dd0d910f
Kaushik Deepak Gokhale <kaushik.gokhale@waiin.com>
2023-12-18 12:16:11 +0530

    Added Code for Backend and Frontend

---
+++ pundit-backend/index.js
@@ -0,0 +1,38 @@
+import { Configuration, OpenAIApi } from "openai";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const configuration = new Configuration({
+  organization: "org-fTzKzWnrf2F0OkyTWmlpjvvd",
+  apiKey: "sk-5h4nQJ4B8qr4mXSJV76NT3BlbkFJOa1Gj0ELXAchRq6Cdd",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a graphic designer gpt",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
